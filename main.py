9d05eba977702b4a46ea0a54a4c34ed50d165b1a
Schwerthecht <schwerthecht@outlook.com>
2023-01-20 06:52:36 +0100

    fix issue with not supporting other translations than en-german

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ import re
 
 
 def use_regex(input_text):
-    pattern = re.compile(r"https:\/\/quizlet\.com\/\w+\/\d+\/[a-z0-9-]+\/", re.IGNORECASE)
-    return pattern.match(input_text)
+    pattern = re.compile(r"https:\/\/quizlet\.com\/\w+\/\d+\/[a-z\d-]+\/", re.IGNORECASE)
+    pattern2 = re.compile(r"https:\/\/quizlet.com\/\d+\/[a-z\d-]+\/", re.IGNORECASE)
+    return pattern.match(input_text) or pattern2.match(input_text)
 
 
 def cancel_if_none(element):
@@ -35,39 +36,37 @@ output_format = buttonbox("Choose an output format", "Output Format", ["txt (cho
 cancel_if_none(output_format)
 
 if output_format == "txt (choose separators)":
-    lang_separator = enterbox(
-        "Enter a language separator (example: \"Hi;Hallo\" -> ; is the language separator)\nuse \\t for tab, "
-        "\\n for new line",
-        "Language Separator", ";")
-    cancel_if_none(lang_separator)
+    word_definition_separator = enterbox(
+        "Enter a word-definition separator (example: \"Hi;Hallo\" -> ; is the word-definition separator)\nuse \\t for "
+        "tab, \\n for new line Word Definition Separator", ";")
+    cancel_if_none(word_definition_separator)
 
-    word_separator = enterbox(
-        "Enter a word separator (example: \"hi;hallo$you;du\" -> $ is the word separator)\nuse \\t for tab, "
-        "\\n for new line",
-        "Language Separator", "\\n")
-    cancel_if_none(word_separator)
+    word_nextword_seperator = enterbox(
+        "Enter a word-nextword separator (example: \"hi;hallo$you;du\" -> $ is the word-nextword separator)\nuse \\t "
+        "for tab, \\n for new line Word Nextword Separator", "\\n")
+    cancel_if_none(word_nextword_seperator)
 
-    lang_separator = lang_separator.replace("\\t", "\t").replace("\\n", "\n")
-    word_separator = word_separator.replace("\\t", "\t").replace("\\n", "\n")
+    word_definition_separator = word_definition_separator.replace("\\t", "\t").replace("\\n", "\n")
+    word_nextword_seperator = word_nextword_seperator.replace("\\t", "\t").replace("\\n", "\n")
 
 
 # --- get requests --- #
 res = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
 
 soup = bs4.BeautifulSoup(res.text, 'html.parser')
-lang_1_html = soup.find_all('span', class_='TermText notranslate lang-en')
-lang_2_html = soup.find_all('span', class_='TermText notranslate lang-de')
+word_html = soup.find_all('a', class_='SetPageTerm-wordText')
+definition_html = soup.find_all('a', class_='SetPageTerm-definitionText')
 
-lang_1 = [word.text for word in lang_1_html]
-lang_2 = [word.text for word in lang_2_html]
+word = [word.contents[0].text for word in word_html]
+definition = [word.contents[0].text for word in definition_html]
 
-dictionary = dict(zip(lang_1, lang_2))
+dictionary = dict(zip(word, definition))
 
 # --- write to file --- #
 with open(file, "w", encoding="utf-8") as f:
     if output_format == "txt (choose separators)":
         for word in dictionary:
-            f.write(word + lang_separator + dictionary[word] + word_separator)
+            f.write(word + word_definition_separator + dictionary[word] + word_nextword_seperator)
     elif output_format == "txt (Anki)":
         for word in dictionary:
             f.write(word + ";" + dictionary[word] + "\n")
